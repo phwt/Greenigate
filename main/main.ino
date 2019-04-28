0845289f0b76c48367efa91ddecadd05508251a2
phwt <phwt.smwt@gmail.com>
2019-04-28 23:51:57 +0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -8,12 +8,20 @@
 
 #include <BlynkSimpleEsp8266.h>
 
+// #include <EEPROM.h>
+
 int pin_pump = D1;
 int pin_btn = D7;
 int pin_led = D8;
 int pin_soil = A0;
 
 void(* resetFunc) (void) = 0;
+
+// bool shouldSaveConfig = false;
+// void saveConfigCallback () {
+//   Serial.println("Should save config");
+//   shouldSaveConfig = true;
+// }
 
 void pumpOn(int length){
     digitalWrite(pin_pump, HIGH);
@@ -22,8 +30,8 @@ void pumpOn(int length){
 }
 
 WiFiManager wifiManager;
-char blynk_token[34] = "YOUR_TOKEN_HERE";
-WiFiManagerParameter custom_blynk_token("blynk_token", "Blynk Token", blynk_token, 34);
+char blynk_token[34] = "6c4178abbcec4ec0af60edc3c90b2784";
+// WiFiManagerParameter custom_blynk_token("blynk_token", "Blynk Token", blynk_token, 34);
 
 void setup() {
     Serial.begin(115200);
@@ -35,12 +43,21 @@ void setup() {
 
     digitalWrite(pin_led, HIGH);
 
-    wifiManager.addParameter(&custom_blynk_token);
+    // wifiManager.setSaveConfigCallback(saveConfigCallback);
+    // wifiManager.addParameter(&custom_blynk_token);s
     wifiManager.autoConnect("Greenigate-01", "P@ssw0rd");
 
     digitalWrite(pin_led, LOW);
 
-    strcpy(blynk_token, custom_blynk_token.getValue());
+    // if(shouldSaveConfig){
+    //     int eeAddress = 0;
+    //     strcpy(blynk_token, custom_blynk_token.getValue());
+    //     // EEPROM.put(eeAddress, blynk_token);
+    // }
+    
+    // strcpy(blynk_token, EEPROM.get(0, char));
+    Serial.println(blynk_token);
+
     Blynk.begin(blynk_token, WiFi.SSID().c_str(), WiFi.psk().c_str(), "blynk.iot-cm.com", 8080);
 }
 
@@ -59,13 +76,37 @@ void loop() {
     delay(50);
 }
 
+// WidgetLED led1(V5);
+
 //Manual watering
 BLYNK_WRITE(V5) {
     int pinValue = param.asInt();
     digitalWrite(pin_pump, pinValue);
+    // if(pinValue){led1.on();}else{led1.off();}
 }
 
 //Send moisture readings
 BLYNK_READ(V6) {
     Blynk.virtualWrite(V6, map(analogRead(pin_soil), 250, 1024, 0 ,100));
+}
+
+//Read time from application
+BLYNK_WRITE(V7){
+    int pinValue = param.asInt();
+    Blynk.virtualWrite(V7, pinValue);
+    Serial.println(pinValue);
+}
+
+//Read target humidity from application
+BLYNK_WRITE(V8){
+    int pinValue = param.asInt();
+    Blynk.virtualWrite(V8, pinValue);
+    Serial.println(pinValue);
+}
+
+//Read water amount from application
+BLYNK_WRITE(V9){
+    int pinValue = param.asInt();
+    Blynk.virtualWrite(V9, pinValue);
+    Serial.println(pinValue);
 }
